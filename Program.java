73d071108abdf6256b209bc85b5e948c4a889bd8
jsjjbuckley <32102448+jsjjbuckley@users.noreply.github.com>
2017-09-26 15:48:25 -0400

    Ask User for 1-10 guess and compares

--- Program.java
+++ Program.java
@@ -4,12 +4,12 @@ public class Program {
     public static void main(String args[]){
         Random num = new Random();
         Scanner scan = new Scanner(System.in);
-        int userNum = 0;
-        int compNum = 0;
+        int myGuess = 0;
+        int compGuess = 0;
         System.out.println("I'm thinking of a number between 1 and 10. What number am I thinking of???");
-        userNum = scan.nextInt();
-        compNum = num.nextInt(10) + 1;
-        if (userNum == compNum){
+        myGuess = scan.nextInt();
+        compGuess = num.nextInt(10) + 1;
+        if (myGuess == compGuess){
             System.out.println("You must be a mind reader!!");
         }
         else System.out.println("Incorrect. I'm just too unpredictable");
